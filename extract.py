8386d4f61a38cf6aadad41a5cb6fa60e14a29746
julianpulido272 <julianpulido707@gmail.com>
2024-06-02 16:14:16 -0700

    added extract to df feature

--- extract.py
+++ extract.py
@@ -23,7 +23,7 @@ client_id = parser.get("spotify_credentials" , "CLIENT_ID")
 client_secret = parser.get("spotify_credentials" , "CLIENT_SECRET")
 
 
-def getToken():
+def get_token():
   """
   Get a token based on credentials 
   """
@@ -53,13 +53,13 @@ def getToken():
   token = json_result["access_token"]
   return token
 
-def getAuthHeader(token):
+def get_auth_header(token):
   """
   returns a header containing our token for requests 
   """
   return {"Authorization": f"Bearer {token}"}
 
-def getRecentlyPlayed(header):
+def get_recently_played():
   """
   retrives a json list of recently played tracks from yesterday given a user header credentials
   """
@@ -68,29 +68,51 @@ def getRecentlyPlayed(header):
   yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
   #r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = getAuthHeader(header))
-  endpoint = "https://api.spotify.com/v1/me/players/recently-played"
-  r = requests.get(endpoint, headers = getAuthHeader(header))
-  return r
+  #endpoint = "https://api.spotify.com/v1/me/players/recently-played"
+  #r = requests.get(endpoint, headers = getAuthHeader(header))
+  current_playbacks = sp.current_user_recently_played(after = yesterday_unix_timestamp)
+  return current_playbacks
 
-def createUser(client_id, client_secret):
-  sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= client_id,
-                                               client_secret= client_secret,
-                                               redirect_uri= "https://www.google.com",
-                                               scope= "user-library-read"))
-  print("test")
-  return sp
+def extract_relevant_data(current_playback):
+  """
+  Since API returns a boatload of unecessary data, we will 
+  extract the song name, artist, time played, and timestamp of songs
+  """
+  #create a list of size of our current playback
+  size = len(current_playback)
 
-#user = createUser(client_id, client_secret)
+  song_names = []*size
+  artist_names = []*size
+  played_at_list = []*size
+  timestamps = []*size
 
-print (client_id, client_secret)
+  for i in range (size):
+    song_names[i] = (current_playback[i]["track"]["name"])
+    artist_names[i] = (current_playback[i]["track"]["album"]["artists"][0]["name"])
+    played_at_list[i] = (current_playback[i]["played_at"])
+    timestamps[i] = (current_playback["played_at"][0:10])
+        
+    # Prepare a dictionary in order to turn it into a pandas dataframe below       
+  song_dict = {
+      "song_name" : song_names,
+      "artist_name": artist_names,
+      "played_at" : played_at_list,
+      "timestamp" : timestamps
+  }
+  song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
+  return song_df
+      
+
+
+
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= client_id,
                                                client_secret= client_secret,
                                                redirect_uri= "https://www.google.com",
-                                               scope= "user-library-read"))
+                                               scope= "user-read-recently-played"))
   
-
-results = sp.current_user_saved_tracks()
-print(results)
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+results = sp.current_user_recently_played(limit =2)
+print (results)
+print("BREAK\n\n")
+print(results["items"][0]["played_at"])
+#df = extract_relevant_data(results["items"])
+#print(df)
